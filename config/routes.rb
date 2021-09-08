4648745dd3d0767ed38ae01ca293db3eca09d670
Itamar Pereira <itamartpereira@hotmail.com>
2021-09-08 16:34:37 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'areas#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :areas, only: [:index, :show]
 end
