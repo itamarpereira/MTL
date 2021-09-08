75cede188630be7616f46fd4a3beb960aef6cbbb
Itamar Pereira <itamartpereira@hotmail.com>
2021-09-08 16:29:55 -0300

    Update areas controller

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,27 @@
 class AreasController < ApplicationController
+  before_action :set_area, only: [:show] # [:edit, :update, :destroy]
+  before_action :set_areas, only: [:index, :show]
+
+  # GET /areas
+  def index
+  end
+
+  # GET /areas/1
+  def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_areas
+    @areas = Area.all
+  end
+
+  def set_area
+    @area = Area.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def area_params
+    params.require(:area).permit(:name, :description)
+  end
 end
